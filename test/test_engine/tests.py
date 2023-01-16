4c2aad30cdae2782709e3e98d413cd793643989f
Alan299 <Alan299@users.noreply.github.com>
2023-01-16 17:05:36 -0600

    Rewrite unit tests using unittest

--- test/test_engine/tests.py
+++ test/test_engine/tests.py
@@ -1,68 +1,37 @@
-from engine.engine import CapuletEngine
-from engine.engine import WilloughbyEngine
-from engine.engine import SternmanEngine
+import unittest
+from engine.engine import CapuletEngine, WilloughbyEngine, SternmanEngine
 
+class TestEngine(unittest.TestCase):
+    def test_capulet(self):
+        current_mileage = 40000
+        last_service_mileage = 0
+        engine = CapuletEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
+        self.assertTrue(engine.needs_service())
 
+        current_mileage = 30000-1
+        last_service_mileage = 0
+        engine = CapuletEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
+        self.assertFalse(engine.needs_service())
 
-def test_capulet():
+    def test_willoughby(self):
+        current_mileage = 60100
+        last_service_mileage = 0
+        engine = WilloughbyEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
+        self.assertTrue(engine.needs_service())
 
-    current_mileage = 40000
-    last_service_mileage = 0
+        current_mileage = 60000-1
+        last_service_mileage = 0
+        engine = WilloughbyEngine(last_service_mileage=last_service_mileage, current_mileage=current_mileage)
+        self.assertFalse(engine.needs_service())
 
-    engine = CapuletEngine(last_service_mileage= last_service_mileage, current_mileage= current_mileage)
-    
-    assert engine.needs_service() == True, 'True test failed'
+    def test_sternman(self):
+        warning_light_is_on = True
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertTrue(engine.needs_service())
 
-    current_mileage = 30000-1
-    last_service_mileage = 0
+        warning_light_is_on = False
+        engine = SternmanEngine(warning_light_is_on)
+        self.assertFalse(engine.needs_service())
 
-    engine = CapuletEngine(last_service_mileage= last_service_mileage, current_mileage= current_mileage)
-
-
-    assert engine.needs_service() == False, 'False test failed'
-
-def test_willoughby():
-    current_mileage = 60100
-    last_service_mileage = 0
-
-    engine = WilloughbyEngine(last_service_mileage= last_service_mileage, current_mileage= current_mileage)
-    
-    assert engine.needs_service() == True, 'True test failed'
-
-    current_mileage = 60000
-    last_service_mileage = 0
-
-    engine = WilloughbyEngine(last_service_mileage= last_service_mileage, current_mileage= current_mileage)
-
-
-    assert engine.needs_service() == False, 'False test failed'
-
-def test_sternman():
-    warning_light_is_on = True
-
-    engine = SternmanEngine(warning_light_is_on)
-    
-    assert engine.needs_service() == True, 'True test failed'
-
-    warning_light_is_on = False
-
-    engine = SternmanEngine(warning_light_is_on)
-
-
-    assert engine.needs_service() == False, 'False test failed'
-
-
-if __name__ ==  "__main__":
-    print("Capulet  Tests")
-    test_capulet()
-
-    print("Willoughby  Tests")
-    test_capulet()
-
-    print("Sternman Tests")
-    test_capulet()
-
-    print("All tests passed.")
-    print("SUCCESS")
-
-
+if __name__ == "__main__":
+    unittest.main()
