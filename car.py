d69eeb92a9ebaf59568a743864dd5c9c2ebe5839
Alan299 <Alan299@users.noreply.github.com>
2023-01-16 22:05:33 -0600

    Add tires to the car class

--- car.py
+++ car.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from engine import *
+from battery import *
 
 class Serviceable(ABC):
     @abstractmethod
@@ -6,11 +8,12 @@ class Serviceable(ABC):
         pass
 
 class Car(Serviceable):
-    def __init__(self, engine, battery):
+    def __init__(self, engine, battery, tires):
         self.__engine = engine
         self.__battery = battery
+        self.__tires = tires 
 
     def needs_service(self):
-        return self.__engine.needs_service() or self.__battery.needs_service()
+        return self.__engine.needs_service() or self.__battery.needs_service() or self.__tires.needs_service()
 
 
